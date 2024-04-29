6e9b878cd0b2348f8d237dab0072083c78812f4a
Srujan B <srujanb28@gmail.com>
2024-04-30 05:01:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    This Function will return the list of requirements
+    """
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+            
+
+setup(
+
+    name = 'machineLearningProject',
+    version = '0.0.1',
+    author = 'Srujan',
+    author_email = 'srujanb28@gmail.com',
+    packages = find_packages(),
+    insatall_requires = get_requirements('requirements.txt')
+)
