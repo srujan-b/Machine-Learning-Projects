fa4beca2b5e648215d6742e261114f8881e1d98c
Srujan B <srujanb28@gmail.com>
2024-05-04 23:43:59 +0530

    Updated requirements with new lib

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -12,10 +12,8 @@ def error_message_details(error,error_detail:sys):
 class CustomException(Exception):
 
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
+        super.__init__(error_message)
         self.error_message = error_message_details(error_message,error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
-
-
